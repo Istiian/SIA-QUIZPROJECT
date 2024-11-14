8118e550cd57ecc449caafbd0617fa5419d667b8
Mark Christian 'Istian' Forlaje <markchristianforlaje@gmail.com>
2024-11-14 12:38:01 +0800

    x

--- express/routes/QuizRouter.js
+++ express/routes/QuizRouter.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
         } else {
             res.render('QuizGen', { Error: 'ERROR' });
             console.log(response_code);
-        }
+        } 
     } catch (error) {
         try {
             const response = await axios.get(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`);
